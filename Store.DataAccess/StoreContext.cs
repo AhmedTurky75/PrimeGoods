a4d00a1658e25ef9cdcb4e83258e5264f574a37d
Ahmed Turky <Ahmedturky75@gmail.com>
2023-08-12 18:07:09 +0300

    Add project files.

---
+++ Store.DataAccess/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Models;
+
+namespace Store.DataAccess
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
